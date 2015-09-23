e7420d9d5210f1b7ce51aa1cb799cafb249c74f5
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-09-23 15:11:52 +0200

    Added Hoja3, more exercises

---
+++ SistemasGestion/Hoja3/Ejercicio01.py
@@ -0,0 +1,14 @@
+#_*_CODING:UTF-8_*_
+
+lista = ["SGE",3.0,["A","B"],10,"FINAL"]
+
+#print type(lista[2][1])
+#print lista[2][1] 
+#print lista[0:3:1] 
+#print lista[0::3] 
+#print lista[0:4] 
+#print lista[1][1] 
+#lista[2][2]="z"
+#print lista[:]
+#print lista[-3][1]
+#print lista[-1]
---
+++ SistemasGestion/Hoja3/Ejercicio02.py
@@ -0,0 +1,12 @@
+#_*_coding:utf-8_*_
+
+lista = ["A","B","C",1,2,["LUNES","MARTES"]]
+
+#Añade la letra z
+#lista.append(“z”)
+#Indica la longitud de la lista
+#print len(lista)
+#print lista[::4]
+#print lista[0:3:2]
+#print lista[-1][0]
+#print lista[2::1]
