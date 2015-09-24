c9e822d597d635647ca838f116b8c2d22bc23e3c
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-09-24 12:55:07 +0200

    Revert "Delete exercises of Tema0 from ProyectoAD"
    This reverts commit 7293855fbde2bf5df5125627d71217ba0787f220.

---
+++ AccesoDatos/ProyectoAD/src/ProyectoAD/Tema0Ej4.java
@@ -0,0 +1,10 @@
+package ProyectoAD;
+
+public class Tema0Ej4 {
+
+    //Modifica el ejercicio anterior, de tal manera que debemos crear un array
+    //con tres valores fijos  {"Tio", "Suegros", "Abogado"};.
+    //Debemos borrar del Hashmap  los valors que coincidan con estos tres. 
+    //Una vez borrado debemos mostrar el contenido del map mediante el patrón iterator
+
+}
