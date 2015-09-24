38be2647ac69beecbf8e376043f362f5bb41dc8d
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-09-24 12:55:41 +0200

    Delete exercises of Tema0 from ProyectoAD

--- AccesoDatos/ProyectoAD/src/ProyectoAD/Tema0Ej5.java
+++
@@ -1,9 +0,0 @@
-package ProyectoAD;
-
-public class Tema0Ej5 {
-
-    //Creamos una clase animal con el dato Nombre de dichos animales.
-    //Debemos crear 3 objetos animal, teniendo en cuenta que  nos dicen que
-    //no debemos aceptar animales repetidos. Indica que tipo de Collections 
-    //usarás y realiza el ejemplo.
-}
