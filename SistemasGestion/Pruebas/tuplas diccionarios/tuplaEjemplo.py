1c30d2f143dd3ff82fbb6f1bb85e5d8147dd3848
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-09-29 19:25:53 +0200

    Added two examples of tuplas

---
+++ SistemasGestion/Pruebas/tuplas diccionarios/tuplaEjemplo.py
@@ -0,0 +1,11 @@
+#_*_coding:utf-8_*_
+
+tupla = (2,4,6,8,9)
+suma = 0
+i = 0
+
+while i < len(tupla) :
+	suma += tupla[i]
+	i += 1
+
+print suma
---
+++ SistemasGestion/Pruebas/tuplas diccionarios/tuplaejemplo2.py
@@ -0,0 +1,5 @@
+#_*_coding:utf-8_*_
+
+lista = ['a','b','c','d']
+for i in range(len(lista)) :
+	print (i,lista[i])
