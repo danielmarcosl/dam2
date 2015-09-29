693ce95a5d2a1cfc7c1a83362a69313c098d7ede
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-09-29 16:41:41 +0200

    Added exercises done

---
+++ SistemasGestion/Hoja3/Ejercicio04.py
@@ -0,0 +1,35 @@
+#_*_coding:utf-8_*_
+
+# Caso 1
+#lista1 = [2,4,6,8]
+#lista2 = [10,12,14,16]
+#listatotal = lista1 + lista2
+#print listatotal
+#La lista2 se suma al final de la lista1, [2,4,6,8,10,12,14,16]
+
+# Caso 2
+#lista1 = ['juan','carlos']
+#producto = lista1 * 3
+#print producto
+#La lista se multiplica por tres, ['juan','carlos','juan','carlos','juan','carlos']
+
+# Caso 3
+#lista = [2,4,6,8,10]
+#print lista[2:4]
+# Muestra la posicion 2 y 3 [6,8]
+#print lista[:3]
+# Se muestra hsata la posicion 3, [2,4,6]
+#print lista[3:]
+# Se muestra desde la posicion 3, [8,10]
+#lista[1] = 10
+#print lista
+# La posicion 1 deja de ser 4 y se convierte en 10, [2,10,6,8,10]
+
+# Caso 4
+#lista1 = [12,45,1,2,5,4,3,55]
+#if 5 in lista1:
+#	print 'El valor 1 esta contenido en la lista'
+#else:
+#	print 'El valor 1 no está contenido en la lista'
+#print lista1
+# Como 5 esta en la lista, se muestra el contenido del if
---
+++ SistemasGestion/Hoja3/Ejercicio05.py
@@ -0,0 +1,17 @@
+#_*_coding:utf-8_*_
+
+tupla = [2,4,6,8,10]
+
+# Mostrar el primer elemento
+print tupla[0]
+# Mostrar el ultimo elemento
+print tupla[-1]
+# Modificar el 8 por un 20
+tupla[3] = 20
+print tupla
+# Modificar en un solo comando el 4 y el 6 por a y b
+#tupla[1] = 'a' and tupla[2] = 'b'
+print tupla
+# Borrar la b con el comando del
+del(tupla[2])
+print tupla
