23d97201eee4d71ef648effa7d8f07ed81f1976b
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-10-07 18:13:47 +0200

    Added more exercises of testing

--- SistemasGestion/Python/Pruebas/diccionarios/ejercicio.py
+++ SistemasGestion/Python/Pruebas/diccionarios/ejercicio.py
@@ -3,4 +3,7 @@
 tupla = tuple([01,02,03,04,05])
 cadena = "hola esta pantalla da sida"
 
+diccionario = dict()
 
+for i in tupla :
+	print i
---
+++ SistemasGestion/Python/Pruebas/diccionarios/methodsandfunctions.py
@@ -0,0 +1,16 @@
+#_*_coding:utf-8_*_
+
+#diccionario = dict()
+
+diccionario = {1:"uno",2:"dos",3:"tres"}
+
+#del diccionario
+
+#diccionario2 = diccionario.copy()
+#print cmp(diccionario,diccionario2)
+
+lista = [7,8,9]
+
+prueba = diccionario.fromkeys([7,8,9],"siete")
+
+print diccionario.items()
