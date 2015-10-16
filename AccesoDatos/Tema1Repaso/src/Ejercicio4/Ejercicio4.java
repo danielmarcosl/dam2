33a844b674f64f62bcfd9ad0117e475d8396df2a
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-10-16 21:15:04 +0200

    Added exercise for todo

---
+++ AccesoDatos/Tema1Repaso/src/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,35 @@
+package Ejercicio4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class Ejercicio4 {
+
+    public static void main(String args[]) {
+
+        System.out.println("1) Volcado de ArrayList\n2) Mostrar el numero de la posicion 3\n3) Salir");
+
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+        int opcion = 0;
+        
+        try {
+            opcion = Integer.parseInt(teclado.readLine());
+        } catch (IOException e) {
+            System.out.println("Error; " + e);
+        }
+
+        switch (opcion) {
+            case 1:
+                //TODO
+                break;
+            case 2:
+                break;
+        }
+    }
+}
