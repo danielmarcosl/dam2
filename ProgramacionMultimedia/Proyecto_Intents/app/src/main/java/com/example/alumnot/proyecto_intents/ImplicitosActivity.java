f5be71b0085157105a8d91c8d77a9a2a7c3dcd9d
Daniel <danielmarcos.to@gmail.com>
2015-10-20 21:26:36 +0200

    Update app

--- ProgramacionMultimedia/Proyecto_Intents/app/src/main/java/com/example/alumnot/proyecto_intents/ImplicitosActivity.java
+++ ProgramacionMultimedia/Proyecto_Intents/app/src/main/java/com/example/alumnot/proyecto_intents/ImplicitosActivity.java
@@ -1,10 +1,18 @@
 package com.example.alumnot.proyecto_intents;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Locale;
 
 /**
  * Created by Daniel Marcos Lorrio on 15/10/15.
@@ -37,7 +45,7 @@ public class ImplicitosActivity extends Activity {
 
         if (imagePath != null) {
             Log.i("myApp", "onPause");
-            file imgToDelete = new file(imagePath);
+            File imgToDelete = new File(imagePath);
             imgToDelete.delete();
         }
     }
@@ -56,6 +64,65 @@ public class ImplicitosActivity extends Activity {
                 i = new Intent(Intent.ACTION_VIEW,
                         Uri.parse("http://www.google.com"));
                 startActivity(Intent.createChooser(i, "Select web app"));
+                break;
+            case R.id.verContactos:
+                i = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("content://contacts/people/"));
+                startActivity(i);
+                break;
+            case R.id.llamar:
+                i = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("tel:657486789"));
+                startActivity(i);
+                break;
+            case R.id.google:
+                i = new Intent(Intent.ACTION_WEB_SEARCH);
+                i.putExtra(SearchManager.QUERY, "android");
+                startActivity(i);
+                break;
+            case R.id.mail:
+                i = new Intent(Intent.ACTION_SEND);
+                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"test@gmail.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "asunto del corero");
+                i.putExtra(Intent.EXTRA_TEXT, "cuerpo del mensaje");
+                startActivity(Intent.createChooser(i, "Select email app"));
+                break;
+            case R.id.mapas:
+                try {
+                    Class.forName("com.google.android.maps.MapActivity"); // Comprobamos si el servicio de mapas está presente en el telefono, en principio todos los tienen
+
+                    float latitud = 40.423806f;
+                    float longitud = -3.670431f;
+                    int zoom = 15;
+
+                    String geoURI = String.format(Locale.US,
+                            "geo:%f,%f=z=%d", latitud, longitud, zoom);
+
+                    i = new Intent(Intent.ACTION_VIEW, Uri.parse(geoURI));
+
+                    startActivity(i);
+                } catch (Exception e) {
+                    Toast toast = Toast.makeText(this,
+                            "No tienes Google APIs",
+                            Toast.LENGTH_LONG);
+                    toast.show();
+
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.galeria:
+                i = new Intent(Intent.ACTION_PICK);
+                i.setType("image/*");
+                startActivityForResult(i, RC_SELECT_PHOTO);
+                break;
+            case R.id.camara:
+                i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                imagePath = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/tmp_image.jpg";
+                File tmpFile = new File(imagePath);
+                Uri uri = Uri.fromFile(tmpFile);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(i, RC_TAKE_PHOTO);
                 break;
         }
     }
