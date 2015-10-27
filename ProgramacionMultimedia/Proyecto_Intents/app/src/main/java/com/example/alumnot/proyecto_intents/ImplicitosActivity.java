fd36b7c76c53c06a4410c49d9e2d668c3701cf5c
Daniel <danielmarcos.to@gmail.com>
2015-10-27 21:12:54 +0100

    update and finish project

--- ProgramacionMultimedia/Proyecto_Intents/app/src/main/java/com/example/alumnot/proyecto_intents/ImplicitosActivity.java
+++ ProgramacionMultimedia/Proyecto_Intents/app/src/main/java/com/example/alumnot/proyecto_intents/ImplicitosActivity.java
@@ -1,158 +1,300 @@
 package com.example.alumnot.proyecto_intents;
 
+import java.io.File;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.MediaStore.MediaColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.util.Locale;
+public class ImplicitosActivity extends Activity{
 
-/**
- * Created by Daniel Marcos Lorrio on 15/10/15.
- */
-public class ImplicitosActivity extends Activity {
+	
+	//Miembros de la clase ()
+	private final int RC_SELECT_PHOTO = 1;
+	private final int RC_TAKE_PHOTO = 2;
+	
+	private String imagePath = null;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_implicitos);
+	}
 
-    // Miembros de la clase
-    private final int RC_SELECT_PHOTO = 1;
-    private final int RC_TAKE_PHOTO = 2;
+	
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+		if(imagePath != null){
+			Log.i("myApp","onPause");
+			File imgToDelete = new File(imagePath);
+			imgToDelete.delete();
+		}
+	}
 
-    // Para manejar las URI de las imagenes
-    private String imagePath = null;
 
-    /**
-     * Metodo oncreate que mostrara el layout
-     *
-     * @param savedInstanceState
-     */
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_implicitos);
-    }
 
-    @Override
-    /**
-     * Metodo onPause que borrara la imagen al minimizar la aplicacion
-     */
-    protected void onPause() {
-        super.onPause();
 
-        if (imagePath != null) {
-            Log.i("myApp", "onPause");
-            File imgToDelete = new File(imagePath);
-            imgToDelete.delete();
-        }
-    }
 
-    /**
-     * Metodo que se ejecutara cada vez que se pulse un boton
-     * Create un nuevo Intent con una URI distinta segun el boton que se pulse
-     *
-     * @param v
-     */
-    public void lanzarActivity(View v) {
-        Intent i;
 
-        switch (v.getId()) {
-            case R.id.verWeb:
-                i = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("http://www.google.com"));
-                startActivity(Intent.createChooser(i, "Select web app"));
-                break;
-            case R.id.verContactos:
-                i = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("content://contacts/people/"));
-                startActivity(i);
-                break;
-            case R.id.llamar:
-                i = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("tel:657486789"));
-                startActivity(i);
-                break;
-            case R.id.google:
-                i = new Intent(Intent.ACTION_WEB_SEARCH);
-                i.putExtra(SearchManager.QUERY, "android");
-                startActivity(i);
-                break;
-            case R.id.mail:
-                i = new Intent(Intent.ACTION_SEND);
-                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"test@gmail.com"});
-                i.putExtra(Intent.EXTRA_SUBJECT, "asunto del corero");
-                i.putExtra(Intent.EXTRA_TEXT, "cuerpo del mensaje");
-                startActivity(Intent.createChooser(i, "Select email app"));
-                break;
-            case R.id.mapas:
-                try {
-                    Class.forName("com.google.android.maps.MapActivity"); // Comprobamos si el servicio de mapas está presente en el telefono, en principio todos los tienen
 
-                    float latitud = 40.423806f;
-                    float longitud = -3.670431f;
-                    int zoom = 15;
 
-                    String geoURI = String.format(Locale.US,
-                            "geo:%f,%f=z=%d", latitud, longitud, zoom);
 
-                    i = new Intent(Intent.ACTION_VIEW, Uri.parse(geoURI));
 
-                    startActivity(i);
-                } catch (Exception e) {
-                    Toast toast = Toast.makeText(this,
-                            "No tienes Google APIs",
-                            Toast.LENGTH_LONG);
-                    toast.show();
 
-                    e.printStackTrace();
-                }
-                break;
-            case R.id.galeria:
-                i = new Intent(Intent.ACTION_PICK);
-                i.setType("image/*");
-                startActivityForResult(i, RC_SELECT_PHOTO);
-                break;
-            case R.id.camara:
-                i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                imagePath = Environment.getExternalStorageDirectory().getAbsolutePath() +
-                        "/tmp_image.jpg";
-                File tmpFile = new File(imagePath);
-                Uri uri = Uri.fromFile(tmpFile);
-                i.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-                startActivityForResult(i, RC_TAKE_PHOTO);
-                break;
-        }
-    }
+	public void lanzarActivity(View v){
+		
+		Intent i;
+		
+		switch (v.getId()) {
+		
+		case R.id.verWeb:
+			i = new Intent(Intent.ACTION_VIEW, 
+					Uri.parse("http://www.google.com"));
+			//startActivity(i);
+			startActivity(Intent.createChooser(i, "Select web App"));
+			break;
+			
+			
+		case R.id.verContactos:	
+			i = new Intent(Intent.ACTION_VIEW, 
+					Uri.parse("content://contacts/people/"));
+			startActivity(i);
+			break;
+			
+		case R.id.llamar:	
+			i = new Intent(Intent.ACTION_VIEW, 
+					Uri.parse("tel:6574837475"));
+			startActivity(i);
+			break;
+		
+		case R.id.google:	
+			i = new Intent(Intent.ACTION_WEB_SEARCH);
+			
+			i.putExtra(SearchManager.QUERY, "android");
+			
+			startActivity(i);
+			
+			break;
+			
+			
+		case R.id.mail:	
+			i = new Intent(Intent.ACTION_SEND);
+			
+			i.setType("message/rfc822"); //type MIME de los Mail
+			
+			//Array de strings que acumula los mail de destino
+			i.putExtra(Intent.EXTRA_EMAIL, new String[]{"test@gmail.com"});
+			i.putExtra(Intent.EXTRA_SUBJECT, "asunto del correo");
+			i.putExtra(Intent.EXTRA_TEXT, "cuerpo del correo");
+			
+			//startActivity(i);
+			startActivity(Intent.createChooser(i, "Select email App"));
+			break;
+			
+			
+			
+		case R.id.mapas:
+			
+			try{
+				//Pregunta si tiene instalado el sistema de google Maps
+				//del paquete GOOGLE API�s
+				Class.forName("com.google.android.maps.MapActivity");
+				//Si esta linea da error, pasa directamente al catch
+				
+				float latitud = 40.423806f;
+				float longitud = -3.670431f;
+				int zoom = 15;
+				
+				String geoURI = String.format(Locale.US, 
+						"geo:%f,%f?z=%d", latitud,longitud,zoom);
+				
+				i = new Intent(Intent.ACTION_VIEW, Uri.parse(geoURI));
+				startActivity(i);
+				
+			}
+			catch(Exception e){
+				Toast toast = Toast.makeText(this, 
+						"No tienes Google Apis", 
+						Toast.LENGTH_LONG);
+				toast.show();	
+			}
+			break;
+			
+			
+		case R.id.galeria:
+			i = new Intent(Intent.ACTION_PICK);
+			i.setType("image/*");
+			//startActivity(i);
+			startActivityForResult(i, RC_SELECT_PHOTO);
+			
+			break;
+		
+			
+			
+		case R.id.camara:
+			
+			i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			
+			imagePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp_image.jpg"; //fichero temporal
+			
+			File tmpFile = new File(imagePath);
+			
+			Uri uri = Uri.fromFile(tmpFile);
+			
+			i.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, uri);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case RC_SELECT_PHOTO:
-                if (resultCode == RESULT_OK) {
-                    ImageView imageView = (ImageView) findViewById(R.id.contenedorImagen);
-                    Uri imageUri = data.getData();
-                    Bitmap galleryPick = scaleBitmap(getPathFromUri(imageUri), imageView.getHeight());
+			//startActivity(i);
+			startActivityForResult(i, RC_TAKE_PHOTO);
+			
+			break;
+		}
+	}
 
-                    if (galleryPick != null) {
-                        imageView.setImageBitmap(galleryPick);
-                    } else {
-                        Toast toast = Toast.makeText(this, "fallo al cargar la imagen", Toast.LENGTH_LONG);
-                        toast.show();
-                    }
-                }
-                break;
-            case RC_TAKE_PHOTO:
-                if(resultCode == RESULT_OK) {
-                    ImageView imageView = (ImageView) findViewById(R.id.contenedorImagen);
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
 
-                }
-                break;
-        }
-    }
+		switch (requestCode) {
+		
+		case RC_SELECT_PHOTO:
+			if(resultCode == RESULT_OK){ //RESULT_OK -> es un ENUM
+			
+				ImageView imageView = (ImageView) findViewById(R.id.contenedorImagen);
+				
+				Uri imageUri = data.getData();
+				
+				//Log.i("myApp", getPathFromUri(imageUri));
+				
+				Bitmap galleryPick = scaleBitmap( getPathFromUri(imageUri), imageView.getHeight());
+				
+				if(galleryPick != null){
+					imageView.setImageBitmap(galleryPick);
+				}
+				else{
+					Toast toast = Toast.makeText(this, "fallo al cargar la imagen", Toast.LENGTH_LONG);
+					toast.show();
+				}
+			}
+			
+			break;
+			
+			
+		case RC_TAKE_PHOTO:
+			if(resultCode == RESULT_OK){ //RESULT_OK -> es un ENUM
+				
+				ImageView imageView = (ImageView) findViewById(R.id.contenedorImagen);
+				
+				Bitmap cameraPick = scaleBitmap(imagePath, imageView.getHeight());
+				
+				if(cameraPick != null){
+					imageView.setImageBitmap(cameraPick);
+				}
+				else{
+					Toast toast = Toast.makeText(this, "fallo al cargar la imagen", Toast.LENGTH_LONG);
+					toast.show();
+				}
+				
+			}
+			break;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	//Este metodo convierte una URI generada por el provider MediaStore (base de datos SQL de archivos multimedia)
+	//pj: content://media/external/images/media/13 en un path del sistema de ficheros normal
+	
+	private String getPathFromUri(Uri uri){
+		
+		String path = "";
+		
+		String[] projection = {MediaColumns.DATA};
+		Cursor cursor = getContentResolver().query(uri,projection,null,null,null);
+		
+		try{
+			int column_index = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
+			if(cursor.moveToFirst()){
+				path = cursor.getString(column_index);
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		cursor.close();
+		
+		return path;
+	}
+	
+	
+	
+	private Bitmap scaleBitmap(String imagePath, int maxDimension){
+		
+		Bitmap scaledBitmap; //aqui va la copia de la imagen reescalada
+
+		//--ANALIZAMOS LA IMAGEN
+		
+		//Clase que tiene utilidades para trabajar con bitmaps
+		BitmapFactory.Options op = new BitmapFactory.Options();
+		op.inJustDecodeBounds = true; //solo calcula las dimensiones, no carga el bitmap
+		
+		scaledBitmap = BitmapFactory.decodeFile(imagePath,op);
+
+		
+		//--PREPARAMOS LA PROXIMA ESCALA
+		
+		if((maxDimension < op.outHeight) || (maxDimension < op.outWidth)){
+			op.inSampleSize = Math.round(Math.max((float) op.outHeight / (float) maxDimension, 
+					(float) op.outWidth / (float) maxDimension));
+			//Usamos Math.max porque es mejor que la imagen sea un poco mayor que el ImageView
+			//Ya que si es menor, la agrandara para ajustarla y se podria pixelar
+		}
+		
+		//--APLICAMOS LA NUEVA ESCALA
+		
+		op.inJustDecodeBounds = false; //aqui si carga el  bitmap
+		
+		scaledBitmap = BitmapFactory.decodeFile(imagePath,op);
+	
+		return scaledBitmap;
+	}
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
 }
--- ProgramacionMultimedia/Proyecto_Intents/app/src/main/java/com/example/alumnot/proyecto_intents/MainActivity.java
+++ ProgramacionMultimedia/Proyecto_Intents/app/src/main/java/com/example/alumnot/proyecto_intents/MainActivity.java
@@ -2,26 +2,23 @@ package com.example.alumnot.proyecto_intents;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
-/**
- * Created by Daniel Marcos Lorrio on 15/10/15.
- */
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	
+	public void irActivityListIntents(View v){
+		Intent i = new Intent(this,ImplicitosActivity.class);
+		startActivity(i);
+	}
 
-    public void irActivityListIntents(View v) {
-        Intent i = new Intent(this, ImplicitosActivity.class);
-        startActivity(i);
-    }
+
+
 }
