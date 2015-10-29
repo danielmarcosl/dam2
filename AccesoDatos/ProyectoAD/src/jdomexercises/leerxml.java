a6c2dd8e3b1b6fcca1a049366801e6e01cec81e5
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-10-29 16:49:39 +0100

    Added new test exercise

---
+++ AccesoDatos/ProyectoAD/src/jdomexercises/leerxml.java
@@ -0,0 +1,67 @@
+package jdomexercises;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class leerxml {
+
+    public static void main(String args[]) throws FileNotFoundException, JDOMException, IOException {
+ 
+        // Abrimos el fichero con saxbuilder
+        Document doc = new SAXBuilder().build("C:\\petra\\futbol.xml");
+        
+        // Cogemos el elemento root
+        Element root = doc.getRootElement();
+
+        // Imprimimos el atributo de root
+        System.out.println(root.getAttributeValue("tipo"));
+
+        // Creamos un list con el hijo de root
+        List equipo = root.getChildren("equipo");
+
+        // Lo iteramos
+        Iterator i = equipo.iterator();
+
+        while (i.hasNext()) {
+            Element e = (Element) i.next(); // Recorremos el iterator y cogemos los elements
+            Element club = e.getChild("club"); // Cogemos el hijo club
+            Element presidente = e.getChild("presidente"); // Cogemos el hijo presidente
+
+            System.out.println(club.getName() + " " + club.getText()
+                    + ", valoracion " + club.getAttributeValue("valoracion")
+                    + ", ciudad " + club.getAttributeValue("ciudad"));
+            System.out.println(presidente.getName() + " " + presidente.getText());
+
+            List plantilla = e.getChildren("plantilla"); // Cogemos el hijo plantilla y lo metemos en otra list
+
+            // Iteramos la lista
+            Iterator i2 = plantilla.iterator();
+
+            while (i2.hasNext()) {
+                Element p = (Element) i2.next(); // La recorremos y cogemos los elementos
+                List nombre = p.getChildren("nombre"); // Hacemos una lista con la etiqueta nombre, ya que hay varios
+
+                // Iteramos la lista
+                Iterator i3 = nombre.iterator();
+
+                while (i3.hasNext()) {
+                    Element n = (Element) i3.next(); // Cogemos cada elemento
+                    System.out.println(n.getText()); // Imprimimos el texto que contienen
+                }
+            }
+        }
+    }
+}
