634e1d7abeb4c793473ebcf0966e5cc2a99ac615
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-05 15:52:25 +0100

    Include all exercises in one file

--- AccesoDatos/OperacionesXML/src/Ejercicio1/Ejercicio01.java
+++ AccesoDatos/OperacionesXML/src/Ejercicio1/Ejercicio01.java
@@ -17,7 +17,7 @@ import org.jdom2.output.XMLOutputter;
 public class Ejercicio01 {
 
     public static void main(String args[]) {
-        
+
         // Leemos el XML y lo metemos en un document
         Document doc = null;
         try {
@@ -33,6 +33,56 @@ public class Ejercicio01 {
 
         // Modificamos el atributo precio del hijo nivel
         root.getChild("nivel").setAttribute("precio", "2300");
+
+        // Escribimos las modificaciones
+        try {
+            Herramientas.escribirXML(doc);
+        } catch (IOException ex) {
+            Logger.getLogger(Ejercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Eliminamos el atributo precio del hijo historieta
+        root.getChild("historieta").removeAttribute("precio");
+
+        // Escribimos las modificaciones
+        try {
+            Herramientas.escribirXML(doc);
+        } catch (IOException ex) {
+            Logger.getLogger(Ejercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // Modificamos en root el atributo ubicacion por localidad
+        root.getAttribute("ubicacion").setName("localidad");
+
+        // Escribimos las modificaciones
+        try {
+            Herramientas.escribirXML(doc);
+        } catch (IOException ex) {
+            Logger.getLogger(Ejercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // Modificamos el precio de computadora a 2300
+        root.getChild("computadora").setAttribute("precio","2300");
+
+        // Escribimos las modificaciones
+        try {
+            Herramientas.escribirXML(doc);
+        } catch (IOException ex) {
+            Logger.getLogger(Ejercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // Anadir nueva etiqueta empleado con atributo cargo, con valor responsable
+        root.addContent(new Element("empleado").setAttribute("cargo","empleado"));
+
+        // Escribimos las modificaciones
+        try {
+            Herramientas.escribirXML(doc);
+        } catch (IOException ex) {
+            Logger.getLogger(Ejercicio01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // Eliminar la etiqueta libro
+        root.removeChild("libro");
 
         // Escribimos las modificaciones
         try {
