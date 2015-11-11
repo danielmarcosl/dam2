78200204dcc20f0ad2678db8fa6e00f4c3e895a6
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-11 17:55:00 +0100

    Added exercise1 done

---
+++ SistemasGestion/Python/HojaFunciones/Ejercicio01.py
@@ -0,0 +1,18 @@
+#_*_coding:utf-8_*_
+
+from math import sqrt
+
+def raizcuad(cuad):
+	cuad = sqrt(cuad)
+	print "La raiz cuadrada es: ", cuad
+	
+
+num = 5
+
+print "El numero introducido es: ",num
+
+cuad = num * num
+
+print "El numero elevado al cuadrado es: ", cuad
+
+raizcuad(cuad)
