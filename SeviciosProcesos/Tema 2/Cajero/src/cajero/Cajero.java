24606b3e361efb4795b6f792359de3362afbaf6f
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-11 19:17:40 +0100

    Added new test exercise

---
+++ SeviciosProcesos/Tema 2/Cajero/src/cajero/Cajero.java
@@ -0,0 +1,71 @@
+package cajero;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+class Cuenta {
+
+    private int saldo;
+
+    Cuenta(int s) {
+        saldo = s;
+    }
+
+    int getsaldo() {
+        return saldo;
+    }
+
+    void restar(int cantidad) {
+        saldo = saldo - cantidad;
+    }
+
+    void retirardinero(int cant, String nom) {
+        if (getsaldo() >= cant) {
+            System.out.println(nom + " retira saldo " + cant);
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            restar(cant);
+            System.out.println(nom + " retira " + cant + " actual (" + getsaldo() + ")");
+        } else {
+            System.out.println(nom + " sin saldo suficiente " + getsaldo());
+        }
+
+        if (getsaldo() < 0) {
+            System.out.println("Saldo negativo " + getsaldo());
+        }
+    }
+}
+
+class sacardinero extends Thread {
+
+    private Cuenta c;
+    String nom;
+
+    public sacardinero(String n, Cuenta c) {
+        super(n);
+        this.c = c;
+        this.nom = n;
+    }
+
+    public void run() {
+        for (int i = 0; i <= 4; i++) {
+            c.retirardinero(10, nom);
+        }
+    }
+}
+
+public class Cajero {
+
+    public static void main(String args[]) {
+        Cuenta c = new Cuenta(40);
+        sacardinero h1 = new sacardinero("Golondrino", c);
+        sacardinero h2 = new sacardinero("Petraa", c);
+        h1.start();
+        h2.start();
+    }
+}
