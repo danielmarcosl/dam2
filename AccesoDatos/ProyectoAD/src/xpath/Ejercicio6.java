7188d8972023c60b6bd841e18ad228a8b2066fa3
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-12 17:27:55 +0100

    Added xpath test exercise6

---
+++ AccesoDatos/ProyectoAD/src/xpath/Ejercicio6.java
@@ -0,0 +1,28 @@
+package xpath;
+
+import java.io.IOException;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import Herramientas.herramientas;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class Ejercicio6 {
+
+    public static void main(String args[]) {
+        Document doc = null;
+        try {
+            doc = new SAXBuilder().build("C:\\petra\\cole.xml");
+        } catch (JDOMException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        herramientas.xpath(doc,"/cole/asignatura[@id = '100']/nombre");
+    }
+}
