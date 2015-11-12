dbb873512dbf022ef8b425c6cd38fb5aa15d7432
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-12 17:27:37 +0100

    Added xpath test exercise2

---
+++ AccesoDatos/ProyectoAD/src/xpath/Ejercicio2.java
@@ -0,0 +1,30 @@
+package xpath;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import Herramientas.herramientas;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class Ejercicio2 {
+
+    public static void main(String args[]) {
+        Document doc = null;
+        try {
+            doc = new SAXBuilder().build("C:\\petra\\cole.xml");
+        } catch (JDOMException ex) {
+            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        herramientas.xpath(doc, "//nombre/asignaturas/nombre");
+    }
+}
