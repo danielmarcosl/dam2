6c2de7b399811151b8ed8b9dd220e8f6ca68f5b1
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-19 19:14:26 +0100

    Exercise1 done

--- AccesoDatos/EjemploEmpresaXPATH/src/Parte2/Ejercicio1.java
+++ AccesoDatos/EjemploEmpresaXPATH/src/Parte2/Ejercicio1.java
@@ -1,9 +1,32 @@
 package Parte2;
 
+import java.io.IOException;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import Herramientas.Herramientas;
+
 /**
  *
  * @author Daniel Marcos Lorrio
  */
 public class Ejercicio1 {
-    
+
+    public static void main(String args[]) {
+        Document doc = null;
+
+        try {
+            doc = new SAXBuilder().build("C:\\petra\\ejemploEmpresa.xml");
+        } catch (JDOMException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        // Mostrar el rot del empleado Gonzalez
+        System.out.println("Apartado 1\n");
+
+        Herramientas.xpath(doc, "//child::empleado[child::nombre='Gonzalez']/descendant::*");
+    }
 }
