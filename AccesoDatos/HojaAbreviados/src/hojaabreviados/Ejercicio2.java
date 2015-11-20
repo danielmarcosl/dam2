d54445e43d0683f3483ae3e532d5a5a76e8edadd
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-20 18:58:50 +0100

    Added new exercise of practice

---
+++ AccesoDatos/HojaAbreviados/src/hojaabreviados/Ejercicio2.java
@@ -0,0 +1,58 @@
+package hojaabreviados;
+
+import java.io.IOException;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.Document;
+
+import Herramientas.Herramientas;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Document doc = null;
+        try {
+            doc = new SAXBuilder().build("C:\\petra\\ciclos2.xml");
+        } catch (JDOMException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        //1. Página web del Instituto
+        System.out.println("\nApartado 1\n");
+        Herramientas.xpath(doc, "//child::web");
+
+        //2. Nombre de los Ciclos Formativos
+        System.out.println("\nApartado 2\n");
+        Herramientas.xpath(doc, "//child::ciclo/child::nombre");
+
+        //3. Siglas por las que se conocen los Ciclos Formativos
+        System.out.println("\nApartado 3\n");
+        Herramientas.xpathAttVal(doc, "//child::ciclo", "id");
+
+        //4. Años en los que se publicaron los decretos de título de los Ciclos Formativos
+        System.out.println("\nApartado 4\n");
+        Herramientas.xpathAttVal(doc, "//child::decretoTitulo", "ano");
+
+        //5. Ciclos Formativos de Grado Medio
+        System.out.println("\nApartado 5\n");
+        Herramientas.xpath(doc, "//child::ciclo[child::grado='Medio']/child::nombre");
+
+        //6. Nombre de los Ciclos Formativos de Grado Superior
+        System.out.println("\nApartado 6\n");
+        Herramientas.xpath(doc, "//child::ciclo[child::grado='Superior']/child::nombre");
+
+        //7. Nombre de los Ciclos Formativos anteriores a 2010
+        System.out.println("\nApartado 7\n");
+        Herramientas.xpath(doc, "//child::decretoTitulo[@ano<2010]/../child::nombre");
+
+        //8. Nombre de los Ciclos Formativos de 2008 o 2010
+        System.out.println("\nApartado 8\n");
+        Herramientas.xpath(doc, "//child::decretoTitulo[@ano=2008 or @ano=2010]/../child::nombre");
+    }
+}
