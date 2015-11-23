8f1535cc337c132c61d16ab5f6f1ffdab4466a4a
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-23 19:32:07 +0100

    Up

--- AccesoDatos/HojaAbreviados/src/hojaabreviados/Ejercicio4.java
+++ AccesoDatos/HojaAbreviados/src/hojaabreviados/Ejercicio4.java
@@ -5,6 +5,8 @@ import org.jdom2.Document;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
+import Herramientas.Herramientas;
+
 /**
  *
  * @author Daniel Marcos Lorrio
@@ -12,15 +14,31 @@ import org.jdom2.input.SAXBuilder;
 public class Ejercicio4 {
 
     public static void main(String args[]) {
-        
+
         Document doc = null;
-        
+
         try {
-            doc = new SAXBuilder().build("asdf");
+            doc = new SAXBuilder().build("C:\\petra\\ej4.xml");
         } catch (JDOMException ex) {
             ex.printStackTrace();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
+        //Nombre de los módulos que se imparten en el Instituto
+        System.out.println("\nApartado 1\n");
+        Herramientas.xpath(doc, "//child::modulo[/child::ciclo=//child::ciclo[/child::grado='Superior']/Attribute::id]/child::nombre");
+
+        //Nombre de los módulos que se imparten en el Instituto
+        System.out.println("\nApartado 2\n");
+        //Herramientas.xpath(doc, "//child::modulo/child::nombre");
+
+        //Nombre de los módulos que se imparten en el Instituto
+        System.out.println("\nApartado 3\n");
+        //Herramientas.xpath(doc, "//child::modulo/child::nombre");
+
+        //Nombre de los módulos que se imparten en el Instituto
+        System.out.println("\nApartado 4\n");
+        //Herramientas.xpath(doc, "//child::modulo/child::nombre");
     }
 }
