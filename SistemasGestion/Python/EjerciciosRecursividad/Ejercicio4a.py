f9d9e166f5ad248753e643c588d8df2c3478e013
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-24 19:23:48 +0100

    Added other form of exercise 4

---
+++ SistemasGestion/Python/EjerciciosRecursividad/Ejercicio4a.py
@@ -0,0 +1,11 @@
+#_*_coidng:utf-8_*_
+
+def pares(lista) :
+	if lista == [] :
+		return []
+	elif lista[0] % 2 == 0 :
+		return [lista[0]] + pares(lista[1:])
+	else :
+		return pares(lista[1:])
+		
+print pares ([1,2,3,4,5,6,7,8,9])
