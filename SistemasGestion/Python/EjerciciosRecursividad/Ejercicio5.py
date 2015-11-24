26761fb59a6581741b9a75615b27f945bb7e27eb
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-24 19:23:54 +0100

    Added exercise 5

---
+++ SistemasGestion/Python/EjerciciosRecursividad/Ejercicio5.py
@@ -0,0 +1,12 @@
+#_*_coding:utf-8_*_
+
+def concatenar(L,M) :
+	if L == [] :
+		return [] + M
+	else :
+		return [L[0]] + concatenar(L[1:],M)
+
+L = [1,2,3,4,5]
+M = [6,7,8,9,0]
+
+print concatenar(L,M)
