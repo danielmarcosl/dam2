68a963c5ab77508129cbdf635464d6657c69690e
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-25 19:25:10 +0100

    Added exercise 7

---
+++ AccesoDatos/Practica5/src/Ejercicios/Ejercicio07.java
@@ -0,0 +1,30 @@
+package Ejercicios;
+
+import java.io.IOException;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import Herramientas.Herramientas;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class Ejercicio07 {
+
+    public static void main(String[] args) {
+        Document doc = null;
+
+        try {
+            doc = new SAXBuilder().build("C:\\petra\\universidad.xml");
+        } catch (JDOMException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        // Códigos de las carreras en las que hay algún alumno matriculado
+        Herramientas.xpathAttVal(doc, "//alumno//carrera[@codigo]", "codigo");
+    }
+}
