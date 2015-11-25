6cea022c152d0999df79a8e1fda0deab30bf1575
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-25 19:25:37 +0100

    Added exercise 15

---
+++ AccesoDatos/Practica5/src/Ejercicios/Ejercicio15.java
@@ -0,0 +1,30 @@
+package Ejercicios;
+
+import java.io.IOException;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import Herramientas.Herramientas;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class Ejercicio15 {
+
+    public static void main(String[] args) {
+        Document doc = null;
+
+        try {
+            doc = new SAXBuilder().build("C:\\petra\\universidad.xml");
+        } catch (JDOMException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        // Nombre de las asignaturas que estudia Luisa
+        Herramientas.xpath(doc, "//asignatura[@id=//alumno[nombre='Luisa']//asignatura/@codigo]/nombre");
+    }
+}
