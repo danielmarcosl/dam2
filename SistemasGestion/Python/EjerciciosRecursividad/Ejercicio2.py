63f461f915b01057a70cb0ede001be8f1dd65da9
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-25 16:57:25 +0100

    Added exercise 2

---
+++ SistemasGestion/Python/EjerciciosRecursividad/Ejercicio2.py
@@ -0,0 +1,13 @@
+#_*_coding_utf-8_*_
+
+def pos(k,L) :
+	if L == [] :
+		return False
+	elif k == 0 :
+		return L[0]
+	else :
+		return pos(k-1, L[1:])
+		
+lista = [1,2,3,4,5,6]
+
+print pos(2,lista)
