73fef98b2513d944393196f720f71bcde0d9f92b
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-12-04 21:04:51 +0100

    Deleted import

--- AccesoDatos/Practica2/src/Ejercicio2/Ejercicio2.java
+++ AccesoDatos/Practica2/src/Ejercicio2/Ejercicio2.java
@@ -2,7 +2,6 @@ package Ejercicio2;
 
 import Herramientas.HerramientasEjercicio1;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
