9fca69d20e6684f06b3611c4ccbe8bd3893181c7
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-08 16:11:39 +0100

    Added client

--- SeviciosProcesos/Tema 3/ServerSocketStream/src/serversocketstream/ClientSocketStream.java
+++ SeviciosProcesos/Tema 3/ServerSocketStream/src/serversocketstream/ClientSocketStream.java
@@ -7,20 +7,30 @@ public class ClientSocketStream {
 
     public static void main(String[] args) {
         try {
-            Socket clientSocket = new Socket(); //Creando socket cliente
-            
+
+            //Creando socket cliente
+            Socket clientSocket = new Socket();
+
             //Estableciendo conexión
-            InetSocketAddress addr = new InetSocketAddress("localhost", 1027);
+            InetSocketAddress addr = new InetSocketAddress("192.168.35.185", 1027);
             clientSocket.connect(addr);
-            
+
             //enviando mensaje
             InputStream is = clientSocket.getInputStream();
             OutputStream os = clientSocket.getOutputStream();
-            String mensaje = "mensaje desde el cliente";
-            os.write(mensaje.getBytes());
-            
+            String mensaje = "";
+            PrintWriter fsalida = new PrintWriter(os, true);
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+            while (!mensaje.equals("cerrar")) {
+                System.out.print("Introduce argo: ");
+                mensaje = teclado.readLine();
+                fsalida.println(mensaje);
+            }
+
             //Mensaje eviado, ahora cierro el socket 
             clientSocket.close();
+            fsalida.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
