4837914eed255769e389296b5c4c019bf8049c05
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-14 19:24:36 +0100

    Added exercise 12

---
+++ SistemasGestion/Python/Segunda Evaluacion/EjerciciosClase1/Ejercicio1.py
@@ -0,0 +1,31 @@
+#_*_coding:utf-8_*_
+
+'''
+Mostrar el contenido de un fichero de texto cuyo nombre es
+introducido por el usuario. Realizar todas las comprobaciones
+pertinentes antes de leerlo línea a línea
+'''
+
+import os
+
+def comprobarFichero(f) :
+	if (f, os.W_OK) :
+		print 'Fichero OK, Escritura OK'
+	elif (f, os.R_OK) :
+		print 'Fichero OK, Lectura OK'
+	else :
+		print 'Fichero no OK'
+
+def pedirDirectorio() :
+	opcion = raw_input("Introduce nombre del archivo: ")
+	return opcion
+
+# main
+
+archivo = open (pedirDirectorio(), "r")
+comprobarFichero(archivo)
+
+print archivo.read()
+
+archivo.close()
+comprobarFichero(archivo)
