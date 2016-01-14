fe979784aa9c24f137347983104876496402fb4a
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-14 19:19:55 +0100

    Added exercise 3 for finished

---
+++ SistemasGestion/Python/Segunda Evaluacion/Actividad1/Ejercicio3.py
@@ -0,0 +1,18 @@
+#_*_coding_utf-8_*_
+
+import os
+
+def comprobarFichero(f) :
+	if (f, os.W_OK) :
+		print 'Fichero OK, Escritura OK'
+	elif (f, os.R_OK) :
+		print 'Fichero OK, Lectura OK'
+	else :
+		print 'Fichero no OK'
+
+def sumador(f) :
+	
+
+# main
+
+archivo = open ('','r')
