905ee4aa936dc0ff165c2716d718c6a65a70a054
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-18 19:07:33 +0100

    Added new project for testing

---
+++ AccesoDatos/ProyectoAD2/src/proyectoad2/ProyectoAD2.java
@@ -0,0 +1,16 @@
+package proyectoad2;
+
+import Herramientas.Herramientas;
+import org.xmldb.api.base.Collection;
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class ProyectoAD2 {
+
+    public static void main(String[] args) {
+        Collection col = Herramientas.connectExistDB("prueba","admin");
+        Herramientas.consultExistDB(col, "for $t in //titulo return $t");
+    }
+    
+}
