ae85a00059be90f95d5dd75e416b2f472c4cf63f
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-19 20:56:31 +0100

    Finished exercise 3

--- SistemasGestion/Python/Segunda Evaluacion/Actividad1/Ejercicio3.py
+++ SistemasGestion/Python/Segunda Evaluacion/Actividad1/Ejercicio3.py
@@ -1,18 +1,24 @@
 #_*_coding_utf-8_*_
 
+'''
+Escribe una funcion sumador(fichero) que, dado el nombre de un
+fichero te texto que contiene numeros separados por espacios
+en blanco, devuelva la suma de dichos numeros
+'''
+
 import os
 
-def comprobarFichero(f) :
-	if (f, os.W_OK) :
-		print 'Fichero OK, Escritura OK'
-	elif (f, os.R_OK) :
-		print 'Fichero OK, Lectura OK'
-	else :
-		print 'Fichero no OK'
-
 def sumador(f) :
+	numeros = f.read().split()
+	suma = 0
 	
+	for i in numeros :	
+		suma += int(i)
+		
+	return suma
 
 # main
 
-archivo = open ('','r')
+f = open ('numeros.txt','r')
+
+print sumador(f)
