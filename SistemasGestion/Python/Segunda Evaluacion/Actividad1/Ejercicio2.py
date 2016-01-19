2cb3f674549008f3f371511123f1753cadbe9aac
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-19 20:39:05 +0100

    Update exercise 2

--- SistemasGestion/Python/Segunda Evaluacion/Actividad1/Ejercicio2.py
+++ SistemasGestion/Python/Segunda Evaluacion/Actividad1/Ejercicio2.py
@@ -1,21 +1,24 @@
 #_*_coding_utf-8_*_
 
-import os
+'''
+Pedimos por teclado un directorio, como no sabemos si lo sera o no.
+Indica que es lo que realmente se ha introducido.
+'''
 
-def comprobarFichero(f) :
-	if (f, os.W_OK) :
-		print 'Fichero OK, Escritura OK'
-	elif (f, os.R_OK) :
-		print 'Fichero OK, Lectura OK'
-	else :
-		print 'Fichero no OK'
+import os
 
 def pedirDirectorio() :
 	opcion = raw_input("Introduce nombre del archivo: ")
 	return opcion
 
+def comprobar(f) :
+	if os.path.isfile(f) == True :
+		print "Es un fichero"
+	elif os.path.isdir(f) == True :
+		print "Es un directorio"
+	else :
+		print "No se sabe"
+
 # main
 
-archivo = open (pedirDirectorio(), "r")
-
-comprobarFichero(archivo)
+comprobar(pedirDirectorio())
