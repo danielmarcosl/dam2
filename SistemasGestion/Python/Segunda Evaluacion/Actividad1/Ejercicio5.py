76a3deba1fdccfcc27d16e06c24a44dbba546246
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-19 21:22:00 +0100

    Added exercise for todo

---
+++ SistemasGestion/Python/Segunda Evaluacion/Actividad1/Ejercicio5.py
@@ -0,0 +1,33 @@
+#_*_coding:utf-8_*_
+
+'''
+Vamos a buscar un caracter en un fichero. Ambos datos los introducimos por
+teclado. Nos debe mostrar un mensaje indicando si lo ha encontrado o no
+'''
+
+import os
+
+def comprobarFichero(f) :
+	if (f, os.W_OK) :
+		print 'Fichero OK, Escritura OK'
+	elif (f, os.R_OK) :
+		print 'Fichero OK, Lectura OK'
+	else :
+		print 'Fichero no OK'
+
+def buscarCaracter(f) :
+	texto = f.read().split()
+	for i in texto :
+		palabras = i.split()
+		for j in palabras :
+			print j
+
+# main
+
+f = open ('ej4.txt','r')
+comprobarFichero(f)
+
+buscarCaracter(f)
+
+f.close()
+comprobarFichero(f)
