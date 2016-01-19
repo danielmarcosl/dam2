8e201c34328081b84c961b5881f1ff909b1e7d29
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-19 19:25:10 +0100

    Added exercise 3 for todo

---
+++ SistemasGestion/Python/Segunda Evaluacion/EjerciciosClase1/Ejercicio3.py
@@ -0,0 +1,7 @@
+#_*_coding:utf-8_*_
+
+'''
+Vamos a crear en python nuestra propia versión del comando COPY,
+para ello copiaremos el contenido de un fichero en otro. Al final,
+mostraremos los dos ficheros y el contenido tiene que ser idéntico.
+'''
