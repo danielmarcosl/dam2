404323274dc031bfa6dc36ebd706374d3e9a6377
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-21 17:02:32 +0100

    Update exercise 2

--- AccesoDatos/XQueryActividadSeguro/src/xqueryactividadseguro/Ejercicio2.java
+++ AccesoDatos/XQueryActividadSeguro/src/xqueryactividadseguro/Ejercicio2.java
@@ -9,6 +9,8 @@ import org.xmldb.api.base.Collection;
 public class Ejercicio2 {
 
     public static void main(String args[]) {
+        // Obtener los nodos numero y tomador de todas las polizas
+
         Collection col = Herramientas.connectExistDB("prueba2", "admin");
         Herramientas.consultExistDB(col, "let $a := //numero\n"
                 + "let $b := //tomador\n"
