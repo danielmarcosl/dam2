85ee756743e35534803a73bcbf2bf33955ede59e
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-21 18:05:47 +0100

    Added exercise 6 done

---
+++ SistemasGestion/Python/Segunda Evaluacion/Actividad1/Ejercicio06.py
@@ -0,0 +1,39 @@
+#_*_coding:utf-8_*_
+
+'''
+Vamos a repetir el ejercicio anterior pero ahora
+a demas nos debera decir en que linea lo ha encontrado
+'''
+
+import os
+
+def comprobarFichero(f) :
+	if (f, os.W_OK) :
+		print 'Fichero OK, Escritura OK'
+	elif (f, os.R_OK) :
+		print 'Fichero OK, Lectura OK'
+	else :
+		print 'Fichero no OK'
+
+def preguntarCaracter() :
+	return str(raw_input("Que caracter quieres buscar?: "))
+
+def buscarCaracter(f) :
+	caracter = preguntarCaracter()
+	
+	texto = f.read().split()
+	for i in texto :
+		letra = list(i)
+		for j in letra :
+			if j == caracter :
+				print "Caracter encontrado en la palabra " + i
+
+# main
+
+f = open ('ej4.txt','r')
+comprobarFichero(f)
+
+buscarCaracter(f)
+
+f.close()
+comprobarFichero(f)
