245c421664496abf7d587763293690974b240252
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-21 16:59:55 +0100

    Added exercise 1 done

---
+++ AccesoDatos/XQueryActividadSeguro/src/xqueryactividadseguro/Ejercicio1.java
@@ -0,0 +1,19 @@
+package xqueryactividadseguro;
+
+import org.xmldb.api.base.Collection;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class Ejercicio1 {
+
+    public static void main(String args[]) {
+        Collection col = Herramientas.connectExistDB("prueba2", "admin");
+        Herramientas.consultExistDB(col, "for $a in //poliza[1]/asegurado\n"
+                + "return <newaseg>"
+                + "{<nombre>{data($a/@nombre)}</nombre> |"
+                + "<apellidos>{data($a/@apellidos)}</apellidos>}"
+                + "</newaseg>");
+    }
+}
