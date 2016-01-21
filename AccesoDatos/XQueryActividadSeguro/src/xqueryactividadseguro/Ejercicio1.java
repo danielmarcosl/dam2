98570865826eefa3e5ced28192d5df7405ce5c92
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-21 17:02:26 +0100

    Update exercise 1

--- AccesoDatos/XQueryActividadSeguro/src/xqueryactividadseguro/Ejercicio1.java
+++ AccesoDatos/XQueryActividadSeguro/src/xqueryactividadseguro/Ejercicio1.java
@@ -9,6 +9,8 @@ import org.xmldb.api.base.Collection;
 public class Ejercicio1 {
 
     public static void main(String args[]) {
+        // Obtener los asegurados de la primera poliza
+
         Collection col = Herramientas.connectExistDB("prueba2", "admin");
         Herramientas.consultExistDB(col, "for $a in //poliza[1]/asegurado\n"
                 + "return <newaseg>"
