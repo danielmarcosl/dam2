e834bbcf99c7a3b5a8e1108ce829a4ffc91ceab5
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-21 18:48:23 +0100

    Finished exercise 8

---
+++ SistemasGestion/Python/Segunda Evaluacion/Actividad1/Ejercicio08.py
@@ -0,0 +1,31 @@
+#_*_coding:utf-8_*_
+
+'''
+Funcion muestra(ruta). Si ruta es un directorio, muestra el contenido del directorio.
+Si es un nombre de archivo, muestra el contenido del archivo en pantalla
+'''
+
+import os
+
+def comprobarFichero(f) :
+	if (f, os.W_OK) :
+		print 'Fichero OK, Escritura OK'
+	elif (f, os.R_OK) :
+		print 'Fichero OK, Lectura OK'
+	else :
+		print 'Fichero no OK'
+
+def muestra(ruta) :
+	if os.path.isdir(ruta) == True :
+		lista = os.listdir(ruta)
+		for i in lista :
+			print i
+	elif os.path.isfile(ruta) == True :
+		with open (ruta,'r') as f :
+			print f.read()
+	else :
+		print "No se sabe"
+
+# main
+
+muestra(raw_input("Introduce una ruta: "))
