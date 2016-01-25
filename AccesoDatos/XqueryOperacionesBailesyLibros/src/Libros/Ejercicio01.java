16d77c5d9c760ae0b6abf016f36827a3edf419b3
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-25 19:22:24 +0100

    Added exercise 1 done

---
+++ AccesoDatos/XqueryOperacionesBailesyLibros/src/Libros/Ejercicio01.java
@@ -0,0 +1,22 @@
+package Libros;
+
+import Herramientas.Herramientas;
+import org.xmldb.api.base.Collection;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class Ejercicio01 {
+
+    public static void main(String args[]) {
+        
+        // Añadir un nuevo atributo en los libros publicados despues de 1999, cambio:”siglo”
+        
+        Collection col = Herramientas.connectExistDB("prueba", "admin");
+        Herramientas.consultExistDB(col, "for $doc in //libro\n"
+                + "let $a := update insert attribute cambio{'siglo'} into $doc\n"
+                + "where $doc/@ano = '1999'\n"
+                + "return $doc");
+    }
+}
