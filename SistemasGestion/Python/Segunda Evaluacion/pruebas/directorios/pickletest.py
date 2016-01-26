0fe97407706f7021414c7018756e7e5f540045d4
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-01-26 18:19:50 +0100

    up

--- SistemasGestion/Python/Segunda Evaluacion/pruebas/directorios/pickletest.py
+++ SistemasGestion/Python/Segunda Evaluacion/pruebas/directorios/pickletest.py
@@ -1,19 +1,19 @@
 #_*_coding:utf-8_*_
 import pickle
 
-lista=['Perl','Python','Ruby']
+lista = ['Perl','Python','Ruby']
 
 # Abrimos un fichero
-archivo=open('C:\petra\ejemplo.dat','wb')
+archivo = open ('C:\petra\ejemplo.dat','wb')
 
 # Método que vuelca la lista al archivo
-pickle.dump(lista,archivo) # carga la lista al archivo
-archivo.close
+pickle.dump (lista,archivo) # carga la lista al archivo
+archivo.close()
 
 # Leemos el archivo
-archivo=open('c:\petra\ejemplo.dat','rb')
+archivo = open ('c:\petra\ejemplo.dat','rb')
 
 # Cargamos la lista desde el archivo
 lista = pickle.load(archivo)
-print(lista)
-archivo.close
+print (lista)
+archivo.close()
