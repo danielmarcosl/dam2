279e473d3b4ef6ea9790ae37c46c377cf10bb769
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-01 19:29:16 +0100

    Finished exercise 3

--- AccesoDatos/XqueryOperacionesBailesyLibros/src/Libros/Ejercicio03.java
+++ AccesoDatos/XqueryOperacionesBailesyLibros/src/Libros/Ejercicio03.java
@@ -8,12 +8,13 @@ import org.xmldb.api.base.Collection;
  * @author Daniel Marcos Lorrio
  */
 public class Ejercicio03 {
-    
-    public static void main (String args[]) {
-        
+
+    public static void main(String args[]) {
+
         // Anadir al nuevo titulo un atributo traduccion="Espanol”
-        
         Collection col = Herramientas.connectExistDB("prueba", "admin");
-        Herramientas.operationExistDB(col, "");
+        Herramientas.operationExistDB(col, "let $doc := //libro/titulo[. = 'TCP/IP Ilustrado']\n"
+                + "let $a := update insert attribute traduccion{'Espanol'} into $doc\n"
+                + "return $doc");
     }
 }
