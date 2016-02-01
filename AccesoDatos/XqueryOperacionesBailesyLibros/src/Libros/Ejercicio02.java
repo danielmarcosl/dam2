7ff56671cf534c3d70aa7afb35b9c106466001e4
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-01 19:28:45 +0100

    up

--- AccesoDatos/XqueryOperacionesBailesyLibros/src/Libros/Ejercicio02.java
+++ AccesoDatos/XqueryOperacionesBailesyLibros/src/Libros/Ejercicio02.java
@@ -8,9 +8,9 @@ import org.xmldb.api.base.Collection;
  * @author Daniel Marcos Lorrio
  */
 public class Ejercicio02 {
-    
+
     public static void main (String args[]) {
-        
+
         // Anadir un nuevo titulo <titulo>TCP/IP ilustrado</titulo> pero solo donde el titulo sea TCP/IP Illustrated
         
         Collection col = Herramientas.connectExistDB("prueba", "admin");
