22aad2ff1ad9b27ee79fb8304fc39bb63082d3bf
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-04 20:40:03 +0100

    Update and added comments

--- ProgramacionMultimedia/PracticaLista/app/src/main/java/com/golondrino/practicalista/MainActivity.java
+++ ProgramacionMultimedia/PracticaLista/app/src/main/java/com/golondrino/practicalista/MainActivity.java
@@ -1,9 +1,12 @@
 package com.golondrino.practicalista;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -13,25 +16,28 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void logIn(View view)
+    private ArrayList<String> headerList = new ArrayList<String>();
+
+    public void listar(View view)
     {
-        EditText nombre = (EditText) findViewById(R.id.Nombre);
-        EditText apellido = (EditText) findViewById(R.id.Apellido);
+        // Declaramos ListView
+        ListView listView = (ListView) findViewById(R.id.lista);
+        // Declaramos EditTexts
+        EditText tNombre = (EditText) findViewById(R.id.Nombre);
+        EditText tApellido = (EditText) findViewById(R.id.Apellido);
 
-        ListView myListView = (ListView) findViewById(R.id.lista);
-        ArrayList<String> myStringArray = new ArrayList<String>();
-        myStringArray.add(nombre.getText().toString());
-        myStringArray.add(apellido.getText().toString());
+        // Anadimos el texto de los EditText al ArrayList
+        headerList.add(String.valueOf(tNombre.getText()));
+        headerList.add(String.valueOf(tApellido.getText()));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        // Vaciar EditTexts
+        tNombre.setText("");
+        tApellido.setText("");
 
-
-        Adapter adapter = new CustomAdapter(getActivity(), R.layout.row, myStringArray1);
-        myListView.setAdapter(adapter);
-
-
-        user.setText(usuariotxt);
-        contr.setText(contrasenatxt);
+        // Linkear el ArrayList y el layout list_item a un ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, headerList);
+        // Anadir el contenido del ArrayAdapter al ListView
+        listView.setAdapter(adapter);
     }// end logIn
 
     @Override
