241e238e3b3c1a5311c184e8d725611cf27ddf06
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-11 20:40:22 +0100

    Update saving the file before to commit :dancer:

--- AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/Ejercicio4descripcionPrecio.sql
+++ AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/Ejercicio4descripcionPrecio.sql
@@ -1,1 +1,21 @@
-CREATE OR REPLACE FUNCTION producto_descripcion_precio
+CREATE OR REPLACE PROCEDURE producto_descripcion_precio(
+num producto.numero%type)
+IS
+	des producto.descripcion%type;
+	pre producto.precio%type;
+BEGIN
+	SELECT descripcion, precio
+	INTO des, pre
+	FROM producto
+	WHERE numero = num;
+	
+	DBMS_OUTPUT.PUT_LINE('Descripcon: '|| des ||', precio'|| pre);
+END producto_descripcion_precio;
+/
+show errors;
+
+SET SERVEROUTPUT ON;
+BEGIN
+	producto_descripcion_precio(7900);
+END;
+/
