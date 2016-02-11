c52e058708ef078e8cfa69a539e0e4afc3a58b22
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-12 00:12:51 +0100

    Indent code

--- AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/Ejercicio2devolverAno.sql
+++ AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/Ejercicio2devolverAno.sql
@@ -1,17 +1,18 @@
 CREATE OR REPLACE FUNCTION devolver_ano(
 	fecha DATE)
-RETURN CHAR IS
+RETURN
+	CHAR
+IS
 	ano CHAR(6);
 BEGIN
 	ano := TO_CHAR(fecha, 'yyyy');
 	RETURN ano;
 END devolver_ano;
 /
-show errors
 
 SET SERVEROUTPUT ON;
 DECLARE
-ano CHAR(6);
+	ano CHAR(6);
 BEGIN
 	ano := devolver_ano(sysdate);
 	DBMS_OUTPUT.PUT_LINE('Ano: '|| ano);
