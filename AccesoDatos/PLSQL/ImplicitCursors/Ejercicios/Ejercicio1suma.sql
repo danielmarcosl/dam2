3af1be89e8095e862dfb5680e1f0468a00c59d07
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-11 17:38:23 +0100

    Added exercise 1 done

---
+++ AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/Ejercicio1suma.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE PROCEDURE sumar_numeros (
+	num1 NUMBER,
+	num2 NUMBER)
+IS
+suma NUMBER;
+BEGIN
+	suma := num1 + num2;
+	DBMS_OUTPUT.PUT_LINE('Suma: '|| suma);
+END sumar_numeros;
+/
+
+SET SERVEROUTPUT ON;
+BEGIN
+	sumar_numeros(4,5);
+END;
+/
