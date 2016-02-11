e84e399761cb0987e7a85b5ba45a81e8eda84d30
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-11 20:05:20 +0100

    Added exercise 3 done

---
+++ AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/Ejercicio3codigoEmpleado.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION buscar_codigo_empleado
+(cod empleado.codigo%type)
+RETURN
+	NUMBER
+IS
+	c empleado.codigo%type;
+BEGIN
+	SELECT codigo into c from empleado where codigo = cod;
+	IF (c LIKE cod) THEN
+		RETURN 1;
+	END IF;
+EXCEPTION
+	WHEN NO_DATA_FOUND THEN
+		RETURN 0;
+END buscar_codigo_empleado;
+/
+
+
+SET SERVEROUTPUT ON;
+DECLARE
+	codigo empleado.codigo%type;
+BEGIN
+	codigo := buscar_codigo_empleado(11);
+	dbms_output.put_line('El valor es'||','|| codigo);
+END;
+/
---
+++ AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/createEmpleado.sql
@@ -0,0 +1,8 @@
+create table empleado(
+codigo number(4) primary key,
+nombre varchar(10),
+apelido varchar(10));
+
+insert into empleado values(11,'Federico','Garcia');
+insert into empleado values(12,'Matias','Prats');
+insert into empleado values(13,'Gufi','Mickey');
