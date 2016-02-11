d237979343b287b5d7c66cd2c23846de25b8fc5d
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-11 19:45:10 +0100

    Modified special characteres

--- SistemasGestion/Python/SegundaEvaluacion/Actividad2/Ejercicio02.py
+++ SistemasGestion/Python/SegundaEvaluacion/Actividad2/Ejercicio02.py
@@ -5,12 +5,12 @@ Se nos pregunta que queremos comer hoy, debemos guardar los platos
 de comida en una lista, pero como no estamos muy conformes con el
 orden en que nos han ido dando los platos vamos a generar una lista
 nueva con una secuencia aleatoria de los platos que guardamos.
-Debemos usar el módulo pertinente y  uno de sus métodos
+Debemos usar el modulo pertinente y uno de sus metodos
 '''
 
 import random
 
-platos = ["Tarta","Fresa","Chocolate","Lays campesinas","Cui a la brasa","Huevo centenario","Patas de gallina"]
+platos = ["Melocotones","Fresas","Chocolate","Lays camperas","Cui a la brasa","Jamon","Kikos"]
 
 platosAleatorios = random.sample(platos, len(platos))
 
