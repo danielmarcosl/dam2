e74f138755e2efb7d81783d0662d2de96c4d4388
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-11 18:19:19 +0100

    Finished exercise 3

--- SistemasGestion/Python/SegundaEvaluacion/Actividad2/Ejercicio03.py
+++ SistemasGestion/Python/SegundaEvaluacion/Actividad2/Ejercicio03.py
@@ -15,25 +15,56 @@ en una lista
 import random
 
 def menu() :
-	print '''Bienvenido al menu del ejercicio 3!
-1. Numeros aleatorios entre 49999 y 99999
-2. Elegir un numero al azar entre los anteriores
-3. Elegir 5 numeros al azar entre los anteriores
-4. Reordenar elementos
-5. Salir'''
+	return '''Bienvenido al menu del ejercicio 3!
+	1. Numeros aleatorios entre 49999 y 99999
+	2. Elegir un numero al azar entre los anteriores
+	3. Elegir 5 numeros al azar entre los anteriores
+	4. Reordenar elementos
+	5. Salir'''
 
 def generarLista(n) :
 	l = list()
-	
+
 	for i in range(n) :
 		l.append(random.randint(49999,99999))
-				
 	return l
+
+def numeroAzar(l) :
+	return random.choice(l)
+
+def variosNumerosAzar(l) :
+	return random.sample(l,5)
+
+def reordenar(l):
+	l.sort()
+	return l
+
+def pedirOpcion() :
+	o = int(raw_input("Escoge una opcion: "))
+	return o
 
 # main
 
-menu()
+print menu()
 
-lista = generarLista(10)
+opcion = 0
+lista = list()
 
-print lista
+while opcion != 5 :
+
+	opcion = pedirOpcion()
+
+	if opcion == 1 :
+		lista = generarLista(10)
+		print lista
+	elif opcion == 2 :
+		print numeroAzar(lista)
+	elif opcion == 3 :
+		print variosNumerosAzar(lista)
+	elif opcion == 4 :
+		lista = reordenar(lista)
+		print lista
+	elif opcion == 5 :
+		print "Gracias por su visita"
+	else :
+		print "Opcion no valida"
