dbbaa6bd7ff33e643b273b2361d788233d252f19
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-12 01:10:33 +0100

    Fix column name

--- AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/createEmpleado.sql
+++ AccesoDatos/PLSQL/ImplicitCursors/Ejercicios/createEmpleado.sql
@@ -1,7 +1,7 @@
 create table empleado(
 codigo number(4) primary key,
 nombre varchar(10),
-apelido varchar(10));
+apellido varchar(10));
 
 insert into empleado values(11,'Federico','Garcia');
 insert into empleado values(12,'Matias','Prats');
