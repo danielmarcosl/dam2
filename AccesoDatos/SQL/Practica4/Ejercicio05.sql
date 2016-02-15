09a338ef4955f4881d7a8ad2b54eed2eab806397
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-15 17:37:08 +0100

    Update enunciate

--- AccesoDatos/SQL/Practica4/Ejercicio05.sql
+++ AccesoDatos/SQL/Practica4/Ejercicio05.sql
@@ -1,5 +1,5 @@
 /*
 Listar las oficinas clasificadas en orden descendente de rendimiento de ventas
-(ventas-objetivos), de modo que las oficinas  con mayor rendimiento aparezcan primero
+(ventas-objetivos), de modo que las oficinas con mayor rendimiento aparezcan primero
 */
 SELECT Oficina, Ciudad, Region, Ventas, Objetivo FROM oficinas ORDER BY (ventas/objetivo) DESC;
