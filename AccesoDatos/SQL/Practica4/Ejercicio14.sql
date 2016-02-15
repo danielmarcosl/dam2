3e9800055cb06d040f781358f00fb6f87c959189
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-15 17:35:25 +0100

    Update enunciate

--- AccesoDatos/SQL/Practica4/Ejercicio14.sql
+++ AccesoDatos/SQL/Practica4/Ejercicio14.sql
@@ -1,5 +1,5 @@
 /*
 Obtener las ciudades donde se encuentran las oficinas con su
-nombre inicial en mayúscula, en minúscula y el número de caracteres
+nombre inicial en mayuscula, en minúscula y el numero de caracteres
 */
 SELECT LOWER(ciudad) AS 'Ciudades min', UPPER(ciudad) AS 'Ciudades may', LENGTH(ciudad) AS 'Caracteres' FROM oficinas;
