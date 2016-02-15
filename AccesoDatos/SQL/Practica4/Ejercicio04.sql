1c215431b54c288a3cef470b5ae9667c10593928
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-15 16:25:17 +0100

    Added exercise 4

---
+++ AccesoDatos/SQL/Practica4/Ejercicio04.sql
@@ -0,0 +1,5 @@
+/*
+Listar las oficinas clasificadas en orden descendente de ventas,
+de modo que las oficinas con mayores  ventas aparezcan en primer lugar
+*/
+SELECT Oficina, Ciudad, Region, Ventas FROM oficinas ORDER BY ventas DESC;
