e2635a1681c2758d8100e81c9b043d331ca829d3
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-15 16:22:50 +0100

    Capitalice 'asc'

--- AccesoDatos/SQL/Practica4/Ejercicio03.sql
+++ AccesoDatos/SQL/Practica4/Ejercicio03.sql
@@ -2,4 +2,4 @@
 Mostrar las ventas de cada oficina ordenada en orden
 alfabético por región y dentro de cada región, por ciudad
 */
-SELECT Ventas, Region, Ciudad FROM oficinas ORDER BY region asc, ciudad asc;
+SELECT Ventas, Region, Ciudad FROM oficinas ORDER BY region ASC, ciudad ASC;
