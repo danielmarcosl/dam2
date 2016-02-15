c831225f2160808ba91fa129054bb957a10fd6e2
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-15 17:36:55 +0100

    Update enunciate

--- AccesoDatos/SQL/Practica4/Ejercicio06.sql
+++ AccesoDatos/SQL/Practica4/Ejercicio06.sql
@@ -1,5 +1,5 @@
 /*
-Listar las oficinas clasificadas en orden alfabético por región y
-dentro de cada región en orden descendente de rendimiento de ventas.
+Listar las oficinas clasificadas en orden alfabetico por region y
+dentro de cada region en orden descendente de rendimiento de ventas.
 */
 SELECT Oficina, Region, Ventas, Objetivo FROM oficinas ORDER BY region ASC, (ventas/objetivo) DESC;
