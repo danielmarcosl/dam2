141fc2847902c158fb455334216dc960e27a0552
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-15 17:36:13 +0100

    Update enunciate

--- AccesoDatos/SQL/Practica4/Ejercicio10.sql
+++ AccesoDatos/SQL/Practica4/Ejercicio10.sql
@@ -1,4 +1,4 @@
 /*
-Mostrar los pedidos que  se han realizado entre 1 mayo y 31 de diciembre de 2004
+Mostrar los pedidos que se han realizado entre 1 mayo y 31 de diciembre de 2004
 */
 SELECT NumPedido, FechaPedido FROM pedidos WHERE fechapedido BETWEEN '2004-05-01' AND '2004-12-31';
