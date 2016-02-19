749eea52ff994767be31ebead5ceca449471d279
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-19 17:06:55 +0100

    Added new project

---
+++ SeviciosProcesos/Tema 4/CifradoFichero/src/cifradofichero/CifradoFichero.java
@@ -0,0 +1,56 @@
+package cifradofichero;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+public class CifradoFichero {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Obteniendo generador de claves con cifrado DES");
+            KeyGenerator keygen = KeyGenerator.getInstance("DES");
+            System.out.println("Generando clave");
+            SecretKey key = keygen.generateKey();
+            System.out.println("Obteniendo objeto Cipher con cifrado DES");
+            Cipher desCipher = Cipher.getInstance("DES");
+            System.out.println("Configurando Cipher para encriptar");
+            desCipher.init(Cipher.ENCRYPT_MODE, key);
+
+            System.out.println("Abriendo el fichero");
+            File inf = new File("fichero_prueba.txt");
+            FileInputStream is = new FileInputStream(inf);
+
+            System.out.println("Abriendo el fichero cifrado");
+            FileOutputStream os = new FileOutputStream("fichero_cifrado");
+            System.out.println("Cifrando el fichero...");
+            
+            byte[] buffer = new byte[8];
+            int bytes_leidos = is.read(buffer);
+
+            while (bytes_leidos != -1) {
+                os.write(desCipher.doFinal(buffer, 0, bytes_leidos));
+                bytes_leidos = is.read(buffer);
+            }
+
+            os.close();
+
+            System.out.println("Obteniendo factoría de claves con cifrado DES");
+            SecretKeyFactory keyfac = SecretKeyFactory.getInstance("DES");
+            System.out.println("Generando keyspec");
+            DESKeySpec keyspec = (DESKeySpec) keyfac.getKeySpec(key, DESKeySpec.class);
+            System.out.println("Salvando la clave en un fichero");
+            FileOutputStream cos = new FileOutputStream("clave");
+
+            cos.write(keyspec.getKey());
+            cos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ SeviciosProcesos/Tema 4/CifradoFichero/src/cifradofichero/FicheroCarga.java
@@ -0,0 +1,45 @@
+package cifradofichero;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+public class FicheroCarga {
+
+    public static void main(String[] args) {
+
+        try {
+            File cinf = new File("clave");
+            FileInputStream cis = new FileInputStream(cinf);
+            byte[] clave = new byte[(int) cinf.length()];
+            cis.read(clave);
+
+            DESKeySpec keyspec = new DESKeySpec(clave);
+            SecretKeyFactory keyfac = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyfac.generateSecret(keyspec);
+            Cipher desCipher = Cipher.getInstance("DES");
+            desCipher.init(Cipher.DECRYPT_MODE, key);
+
+            File inf = new File("fichero_cifrado");
+            FileInputStream is = new FileInputStream(inf);
+            FileOutputStream os = new FileOutputStream("fichero_descifrado.txt");
+            byte[] buffer = new byte[16];
+            int bytes_leidos = is.read(buffer);
+            String mensaje;
+
+            while (bytes_leidos != -1) {
+                os.write(desCipher.doFinal(buffer, 0, bytes_leidos));
+                mensaje = new String(desCipher.doFinal(buffer, 0, bytes_leidos));
+                System.out.print(mensaje);
+                bytes_leidos = is.read(buffer);
+            }
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
