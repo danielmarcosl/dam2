cad8706a1c38917ed56444175f059bc73ee53031
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-19 19:03:36 +0100

    Update exercise 1

--- AccesoDatos/PLSQL/ExplicitCursors/Biblio/Ejercicio1.sql
+++ AccesoDatos/PLSQL/ExplicitCursors/Biblio/Ejercicio1.sql
@@ -1,24 +1,31 @@
-DECLARE CURSOR mostrar_tema_libro2
+CREATE OR REPLACE PROCEDURE mostrar_temas
 IS
-	SELECT tema.nombre, libro.categoria
-	FROM tema
-	JOIN trata_sobre
-	ON tema.clavetema = trata_sobre.clavetema
-	JOIN libro
-	ON trata_sobre.clavelibro = libro.clavelibro
-	WHERE libro.categoria like '%e%e%'
-	AND length(tema.nombre) >= 7;
-
-	nom_tem tema.nombre%type;
-	cat_lib libro.categoria%type;
+	nom_tem tema.nombre%TYPE;
+	cat_lib libro.categoria%TYPE;
+	CURSOR c1
+		IS
+			SELECT tema.nombre, libro.categoria
+			FROM tema
+			JOIN trata_sobre
+			ON tema.clavetema = trata_sobre.clavetema
+			JOIN libro
+			ON trata_sobre.clavelibro = libro.clavelibro
+			WHERE libro.categoria LIKE '%e%e%'
+			AND LENGTH(tema.nombre) >= 7;
 BEGIN
-	OPEN mostrar_tema_libro2;
+OPEN c1;
 	LOOP
-		FETCH mostrar_tema_libro2
+		FETCH c1
 		INTO nom_tem, cat_lib;
-		EXIT WHEN mostrar_tema_libro2%notfound;
+		EXIT WHEN c1%notfound;
 		DBMS_OUTPUT.PUT_LINE('Tema: '||nom_tem||', Categoria: '|| cat_lib);
 	END LOOP;
-	CLOSE mostrar_tema_libro2;
+	CLOSE c1;
+END mostrar_temas;
+/
+
+SET SERVEROUTPUT ON;
+BEGIN
+	mostrar_temas;
 END;
 /
