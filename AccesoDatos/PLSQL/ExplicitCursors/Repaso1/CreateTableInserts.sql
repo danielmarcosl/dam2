56ab2f97d688883ae820d76ee73cb2c8ebe9133c
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-22 19:37:17 +0100

    Update

--- AccesoDatos/PLSQL/ExplicitCursors/Repaso1/CreateTableInserts.sql
+++ AccesoDatos/PLSQL/ExplicitCursors/Repaso1/CreateTableInserts.sql
@@ -1,29 +1,30 @@
 CREATE TABLE alumno(
-id_al INT(4) AUTOINCREMENT PRIMARY KEY,
+id_al NUMBER(4) PRIMARY KEY,
 nombre VARCHAR(20)
 );
 
 CREATE TABLE asignatura(
-id_asig INT(4) AUTOINCREMENT PRIMARY KEY,
+id_asig NUMBER(4) PRIMARY KEY,
 nombre VARCHAR(20)
 );
 
 CREATE TABLE profesor(
-id_prof INT(4) AUTOINCREMENT PRIMARY KEY,
+id_prof NUMBER(4) PRIMARY KEY,
 nombre VARCHAR(20)
 );
 
 CREATE TABLE al_asig(
-id_al INT(4) NOT NULL,
-id_asig INT(4) NOT NULL,
-nota INT(2) NOT NULL,
+id_al NUMBER(4),
+id_asig NUMBER(4),
+nota NUMBER(2) NOT NULL,
 PRIMARY KEY (id_al, id_asig),
 FOREIGN KEY (id_al) REFERENCES alumno(id_al),
 FOREIGN KEY (id_asig) REFERENCES asignatura(id_asig)
 );
 
-INSERT INTO alumno VALUES (null,'');
-INSERT INTO alumno VALUES (null,'');
-INSERT INTO alumno VALUES (null,'');
-INSERT INTO alumno VALUES (null,'');
-INSERT INTO alumno VALUES (null,'');
+INSERT INTO alumno VALUES (1,'Margarito');
+INSERT INTO alumno VALUES (2,'Pepeflutas');
+INSERT INTO alumno VALUES (3,'Diletuqueasino');
+INSERT INTO alumno VALUES (4,'Pofavordesconectemovil');
+INSERT INTO alumno VALUES (5,'Tepidequemetas5');
+
