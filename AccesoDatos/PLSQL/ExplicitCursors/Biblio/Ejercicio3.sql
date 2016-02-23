f48ec0c6ccd856956cc56d09f0d4aa088d9e1e14
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-23 21:34:38 +0100

    Finished exercise 3

--- AccesoDatos/PLSQL/ExplicitCursors/Biblio/Ejercicio3.sql
+++ AccesoDatos/PLSQL/ExplicitCursors/Biblio/Ejercicio3.sql
@@ -1,3 +1,32 @@
-socio.clavesocio
-prestamo.clavesocio
-prestamo.clavelibro
+CREATE OR REPLACE PROCEDURE mostrar_socio_antiguo
+IS
+	nom socio.nombre%TYPE;
+	CURSOR c1
+	IS
+		SELECT nombre
+		FROM socio
+		WHERE clavesocio = (
+			SELECT clavesocio
+			FROM prestamo
+			GROUP BY clavesocio
+			HAVING COUNT(clavesocio) > 1)
+		AND fecha_nac = (
+			SELECT MIN(fecha_nac)
+			FROM socio);
+BEGIN
+	OPEN c1;
+		LOOP
+			FETCH c1
+			INTO nom;
+			EXIT WHEN c1%notfound;
+			DBMS_OUTPUT.PUT_LINE('Nombre :'|| nom);
+		END LOOP;
+	CLOSE c1;
+END mostrar_socio_antiguo;
+/
+
+SET SERVEROUTPUT ON;
+BEGIN
+	mostrar_socio_antiguo;
+END;
+/
