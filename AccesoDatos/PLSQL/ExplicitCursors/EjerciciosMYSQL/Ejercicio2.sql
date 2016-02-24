ce00c5898b68c5f9cb5d26de48c29870aa180c4c
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-24 14:14:58 +0100

    Added exercise 2 done

---
+++ AccesoDatos/PLSQL/ExplicitCursors/EjerciciosMYSQL/Ejercicio2.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS empleados(
+dni VARCHAR(10) PRIMARY KEY,
+nombre VARCHAR(10),
+apellido VARCHAR(10),
+fecha DATE);
+
+INSERT INTO empleados VALUES
+('45456D','Manuel','Galindo','1990-02-21'),
+('41145E','Tochar','Facundo','1988-12-11'),
+('40645C','Santiago','Pachon','1991-06-25'),
+('38875A','Rebeca','Perez','1987-09-13');
+
+DELIMITER ##
+CREATE PROCEDURE pro_apellido_fechas()
+BEGIN
+	DECLARE v_apell VARCHAR(10);
+	DECLARE v_fecha DATE;
+	DECLARE n INT DEFAULT 0;
+	DECLARE i INT DEFAULT 0;
+	DECLARE c_recorrer_empleados CURSOR
+	FOR
+		SELECT apellido, fecha
+		FROM empleados
+		ORDER BY apellido;
+	SELECT COUNT(*) FROM alumnos INTO n;
+	OPEN c_recorrer_empleados;
+		WHILE i < n DO
+			FETCH c_recorrer_empleados
+			INTO v_apell, v_fecha;
+			SELECT concat('Apellido ', v_apell, ', Fecha ', v_fecha)
+			AS 'Apellido, Fecha';
+		END WHILE;
+	CLOSE c_recorrer_empleados;
+END; ##
+DELIMITER ;
