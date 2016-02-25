88a9a50b280175c5407cd49ad11ce01e5c57e29b
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-25 17:43:01 +0100

    Create main class

---
+++ AccesoDatos/SQLite/RepasoSQLite/src/repasosqlite/RepasoSQLite.java
@@ -0,0 +1,15 @@
+package repasosqlite;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daniel Marcos Lorrio
+ */
+public class RepasoSQLite {
+
+    public static void main(String[] args) {
+        // Insertar desde fichero con tokens
+        Herramientas.insertTokenizerSQLite("gurru", "clientes.txt", "cliente");
+    }
+}
