01400fafbc16cbe405197208900f622a5790fc8d
Daniel Marcos <danielmarcos.to@gmail.com>
2016-02-25 06:46:27 +0100

    Added exercise 5 done

---
+++ AccesoDatos/PLSQL/ExplicitCursors/EjerciciosMYSQL/Ejercicio5.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS empleado3(
+dni VARCHAR(10) PRIMARY KEY,
+nombre VARCHAR(15),
+comision INT(3)
+);
+
+CREATE TABLE IF NOT EXISTS copia_emple SELECT * FROM empleado3;
+
+INSERT INTO empleado3 VALUES
+('123','Juan',15),
+('561','Pedro',0),
+('612','Raul',23),
+('717','Sonia',0);
+
+DELIMITER &&
+CREATE TRIGGER tr_borrar_empleado
+AFTER DELETE ON empleado3 FOR EACH ROW
+BEGIN
+	IF old.comision = 0 THEN
+		INSERT INTO copia_emple (dni, nombre, comision)
+		VALUES (old.dni, old.nombre, old.comision);
+	END IF;
+END; &&
+DELIMITER ;
