0e05a4a66c3aa9231e4671c7265203a7175ee012
Daniel Marcos <danielmarcos.to@gmail.com>
2016-03-03 10:51:14 +0100

    Added exercise 3 done

---
+++ SistemasGestion/Python/SegundaEvaluacion/Repaso2/Ej3.py
@@ -0,0 +1,13 @@
+#_*_coding:utf-8_*_
+
+'''
+Escriba el siguiente programa
+'''
+
+import sys
+
+print "El nombre del script: ", sys.argv[0]
+
+print "Cantidad de argumentos: ", len(sys.argv)
+
+print "Los argumentos son: ", str(sys.argv)
